450f26aa05deef7323ea3fe4cf7c33fcc0d807f3
Soldat8889 <lucas.syhanath@gmail.com>
2019-09-30 21:36:36 +0200

    [Add] ServiceWorker Init

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
+import * as serviceWorker from "./serviceWorker";
+
 import App from "./App";
 
 ReactDOM.render(
@@ -10,3 +12,5 @@ ReactDOM.render(
     </BrowserRouter>, 
     document.getElementById("root")
 );
+
+serviceWorker.register();
