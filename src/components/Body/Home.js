c2ef7481c5ea34d6af4b55fb3dd9964e5a4c61fd
Soldat8889 <lucas.syhanath@gmail.com>
2019-10-06 15:46:58 +0200

    [Add] Dark Mode Illustrations

--- src/components/Body/Home.js
+++ src/components/Body/Home.js
@@ -14,20 +14,16 @@ function Home(props) {
                         <section className="Slider__section Slider_wrapper container" data-slide={props.slide}>
                             <div className="Slider__background">
                                 {theme === "light" ?
-                                    <div className="Slider__background-element">
-                                        The DAY
-                                    </div>
+                                    <DayMode />
                                     :
-                                    <div className="Slider__background-element">
-                                        The NIGHT
-                                    </div>
+                                    <NightMode />
                                 }
                             </div>
                             <div className="Slider__title">
                                 <h1 className="Slider__title_text">
                                     <Parallax
-                                        speedX={0.05}
-                                        speedY={0.05}
+                                        speedX={0.08}
+                                        speedY={0.08}
                                         name="Home__title"
                                     >
                                         SYHANATH Lucas <br />
@@ -40,6 +36,66 @@ function Home(props) {
                 </ThemeContext.Consumer>
             )}
         </LanguageContext.Consumer>
+    );
+}
+
+function DayMode() {
+    return (
+        <div className="Slider__background-element">
+            The DAY
+        </div>
+    );
+}
+
+function NightMode() {
+    return (
+        <div id="Slider__background-scene__dark" className="Slider__background-scene" style={{transform: "translateY(100%)"}}>
+            <div className="Slider__background-element Slider__background-element__moon">
+                <Parallax
+                    speedX={0.03}
+                    speedY={0.03}
+                    name="Slider__background-element__moon"
+                >
+                    <img src="/images/moon.svg" alt="Moon" />
+                </Parallax>
+            </div>
+            <div className="Slider__background-element Slider__background-element__stars">
+                <Parallax
+                    speedX={0.01}
+                    speedY={0.01}
+                    name="Slider__background-element__stars"
+                >
+                    <img src="/images/stars.svg" alt="Stars" />
+                </Parallax>
+            </div>
+            <div className="Slider__background-element Slider__background-element__cloud__1">
+                <Parallax
+                    speedX={0.06}
+                    speedY={0.06}
+                    name="Slider__background-element__cloud__1"
+                >
+                    <img src="/images/cloud__1.svg" alt="Cloud 1" />
+                </Parallax>
+            </div>
+            <div className="Slider__background-element Slider__background-element__cloud__2">
+                <Parallax
+                    speedX={0.045}
+                    speedY={0.045}
+                    name="Slider__background-element__cloud__2"
+                >
+                    <img src="/images/cloud__2.svg" alt="Cloud 2" />
+                </Parallax>
+            </div>
+            <div className="Slider__background-element Slider__background-element__cloud__3">
+                <Parallax
+                    speedX={0.055}
+                    speedY={0.055}
+                    name="Slider__background-element__cloud__3"
+                >
+                    <img src="/images/cloud__3.svg" alt="Cloud 3" />
+                </Parallax>
+            </div>
+        </div>
     );
 }
 
